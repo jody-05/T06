2ca01460d8644af1620a49dfd4caa22f990125ec
jody-05 <jodylaw0714@gmail.com>
2025-10-25 17:16:14 +0800

    Update scatterplot.js

--- js/scatterplot.js
+++ js/scatterplot.js
@@ -12,7 +12,7 @@ const drawScatterPlot = (data) => {
     const xExtent = d3.extent(data, d => d.star);
     const yExtent = d3.extent(data, d => d.screenSize);
 
-    const sScaleS = d3.scaleLinear()
+    const xScaleS = d3.scaleLinear()
         .domain([xExtent[0] - 0.5, xExtent[1] + 0.5])
         .range([0, innerWidth]);
 
@@ -24,4 +24,54 @@ const drawScatterPlot = (data) => {
     colorScale 
         .domain(data.map(d => d.screenTech))    // get unique screenTech values
         .range(d3.schemeCategory10);    // use a predefined color scheme
+
+    innerChartS.selectAll("circle")
+        .data(data)
+        .join("circle")
+        .attr("cx", d => xScaleS(d.star))
+        .attr("cy", d => yScaleS(d.screenSize))
+        .attr("r", 4)
+        .attr("fill", d => colorScale(d.screenTech))
+        .attr("opacity", 0.5);
+
+    // Add axes
+    const bottomAxis = d3.axisBottom(xScaleS);
+    innerChartS.append("g")
+        .attr("transform", `translate(0, ${innerHeight})`)
+        .call(bottomAxis);
+
+    svg.append("text")
+        .text("Star Rating")
+        .attr("text-anchor", "end")
+        .attr("x", width - 20)
+        .attr("y", height - 5)
+        .attr("class", "axis-label");
+
+    const leftAxis = d3.axisLeft(yScaleS);
+    innerChartS.append("g")
+        .call(leftAxis);
+
+    svg.append("text")
+        .text("Screen size (inches)")
+        .attr("x", 30)
+        .attr("y", 20)
+        .attr("class", "axis-label");
+
+    // Add a legend on the right-hand side
+    const legend = svg.append('g')
+        .attr('class', 'legend')
+        .attr('transform', `translate(${width - 140}, ${margin.top})`);
+
+    uniqueTechs.forEach((tech, i) => {
+        const g = legend.append('g').attr('transform', `translate(0, ${i * 22})`);
+        g.append('rect')
+            .attr('width', 12)
+            .attr('height', 12)
+            .attr('fill', colorScale(tech));
+        g.append('text')
+            .attr('x', 18)
+            .attr('y', 10)
+            .text(tech)
+            .attr('class', 'axis-label');
+    });
 }
